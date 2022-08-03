c62a086343cb008cbd09e7dc7bd1a1c14768688f
shalabycr7 <cr7.shalaby@gmail.com>
2022-08-03 17:25:42 +0200

    Adding Intit Files

---
+++ src/AppletPaint.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AppletPaint extends Applet {
+    public void paint(Graphics g){
+        g.fillOval(10,10,100,100);
+    }
+}
